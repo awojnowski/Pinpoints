ddc189f5778337bee97f4c8668495ce8bc64e684
Aaron Wojnowski <aaronwojnowski@gmail.com>
2014-04-25 17:57:34 -0500

    Initial Commit

---
+++ Pinpoints/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pinpoints
+//
+//  Created by Aaron Wojnowski on 2014-04-25.
+//  Copyright (c) 2014 aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PPAppDelegate class]));
+    }
+}
